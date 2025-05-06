aa3a4ec81eb6bc6d930dc58bee7d0eadcb234980
dandy <dimash.bakytzhanov@gmail.com>
2025-05-06 08:09:09 +0500

    Prepare for PYTHONPATH env var approach

--- bailanysta_project/manage.py
+++ bailanysta_project/manage.py
@@ -2,14 +2,9 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-from pathlib import Path
-
 
 def main():
     """Run administrative tasks."""
-    PROJECT_ROOT = Path(__file__).resolve().parent.parent
-    sys.path.insert(0, str(PROJECT_ROOT))
-
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bailanysta_project.bailanysta_project.settings')
     try:
         from django.core.management import execute_from_command_line
@@ -20,7 +15,6 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-
 
 if __name__ == '__main__':
     main()
